d3303c7c53690b9867b3af7b66095e49808e8a97
sproott <david.hrabe12@seznam.cz>
2020-11-03 18:51:25 +0100

    Add faster and deterministic JSON stringify

--- src/common/loader/loaderContainer.ts
+++ src/common/loader/loaderContainer.ts
@@ -1,11 +1,12 @@
 import Loader, { LoaderParams } from "./loader"
+import stringify from "fast-json-stable-stringify"
 
 export default class LoaderContainer {
   readonly loaders: Map<string, Loader> = new Map()
 
   getLoader(params: LoaderParams & { fieldName: string }) {
     let { model, fieldName, type } = params
-    let jsonParams = JSON.stringify({ table: model.tableName, fieldName, type })
+    let jsonParams = stringify({ table: model.tableName, fieldName, type })
     return this.loaders.get(jsonParams) || this.createNew(params, jsonParams)
   }
 

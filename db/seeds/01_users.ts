96fc4a0611e63f967c51adc9f04ce0fbf18ea170
sproott <david.hrabe12@seznam.cz>
2021-03-10 14:41:47 +0100

    Change passwords

--- db/seeds/01_users.ts
+++ db/seeds/01_users.ts
@@ -12,19 +12,19 @@ export async function seed(knex: Knex): Promise<void> {
       id: "ee986842-a9a6-44ac-b4e7-42d2a93aae44",
       username: "bob",
       email: "bob33@gmail.com",
-      password: await hash("bruhbruhbob"),
+      password: await hash("usertestbob"),
     },
     {
       id: "19dccdc1-2238-46b5-979b-e97a2176870e",
       username: "john",
       email: "john03@gmail.com",
-      password: await hash("bruhbruhjohn"),
+      password: await hash("usertestjohn"),
     },
     {
       id: "46936727-6e6a-4ba9-a332-30f6230820d5",
       username: "alex",
       email: "alex12@gmail.com",
-      password: await hash("bruhbruhalex"),
+      password: await hash("usertestalex"),
     },
     {
       id: "960bbd7d-c49f-4218-aeba-c237591d2fb8",
